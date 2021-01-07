6b56b4c40b92094a875ef116ec0c6d8a120cbe66
Sam Larsen-Disney <s.larsendisney@gmail.com>
2021-01-07 22:54:26 +0000

    yo

--- app.js
+++ app.js
@@ -37,23 +37,13 @@ var job = new CronJob(
 job.start();
 
 var cors = require("cors");
-var allowedList = [
-  "https://api.sld.codes",
-  "https://sld.codes",
-  "https://log.sld.codes",
-  "http://localhost:8000",
-];
 var corsOptions = {
-  "preflightContinue": true,
-  origin: function (origin, callback) {
-    console.log(`Request from: ${origin}`);
-    if (allowedList.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      console.log(`Unauthorised: ${origin}`)
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: [
+    "https://api.sld.codes",
+    "https://sld.codes",
+    "https://log.sld.codes",
+    "http://localhost:8000",
+  ]
 };
 
 const server = express()
